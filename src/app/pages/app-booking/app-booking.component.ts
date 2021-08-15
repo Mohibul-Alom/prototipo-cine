8205dc275a2f0c1c1f5ca0523aba5b2890269b4f
Mohibul <tuhin28012@gmail.com>
2021-08-15 16:43:08 +0200

    FEAT: added pages: home,logIn,profile,register and booking

---
+++ src/app/pages/app-booking/app-booking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-booking',
+  templateUrl: './app-booking.component.html',
+  styleUrls: ['./app-booking.component.scss']
+})
+export class AppBookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
