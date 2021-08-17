2b6a75e793e2486cd0d6930c32175ad4e39a1aa3
Mohibul <tuhin28012@gmail.com>
2021-08-17 12:04:34 +0200

    FEAT: added home page with movie image

--- src/app/pages/app-home/app-home.component.ts
+++ src/app/pages/app-home/app-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Imovie } from 'src/app/models/iapi';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-app-home',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppHomeComponent implements OnInit {
 
-  constructor() { }
+  public myMoviesList?:Imovie[] = [];
+
+  constructor(private globalService:GlobalService) { }
 
   ngOnInit(): void {
+    this.getMoviesList();
+  }
+
+  public getMoviesList():void {
+    this.globalService.getMovies().subscribe((data:any)=>{
+      
+      data.forEach((element:Imovie)=>{
+
+        let auxMovie:Imovie = this.transformData(element);
+        this.myMoviesList?.push(auxMovie);
+      })
+
+    });
+    console.log("getMovies: ",this.myMoviesList)
+  }
+
+  private transformData(data:Imovie):Imovie{
+
+    const {_id,title,director,description,duration,image,genere} = data;
+
+    let auxMovie:Imovie = {
+      _id,
+      title,
+      director,
+      description,
+      duration,
+      image,
+      genere,
+    }
+
+    return auxMovie;
+
   }
 
 }
