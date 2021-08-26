68605940d4d3f0198d8eb1a538319f7d93fada4d
Mohibul <tuhin28012@gmail.com>
2021-08-26 15:43:45 +0200

    FEAT: added public function to fech the data using booking service

--- src/app/pages/app-booking/components/booking-option/booking-option.component.ts
+++ src/app/pages/app-booking/components/booking-option/booking-option.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Imovie } from 'src/app/models/iapi';
+import { BookingService } from '../../services/booking.service';
 
 @Component({
   selector: 'app-booking-option',
   templateUrl: './booking-option.component.html',
-  styleUrls: ['./booking-option.component.scss']
+  styleUrls: ['./booking-option.component.scss'],
 })
 export class BookingOptionComponent implements OnInit {
+  public name!: string;
+  public movieDetail?: Imovie;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookingService: BookingService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.name = params['name'];
+    });
+
+    this.getMovies(this.name);
+
   }
+
+  public getMovies(name: string):void {
+    this.bookingService.getMoviesByName(name).subscribe(
+      (data: any) => {
+        this.movieDetail = this.transformDataMovie(data[0]);
+        console.log("AQUI ESTA TU PUTA PELICULA-->",this.movieDetail)
+        this.getAuditorium(this.movieDetail._id);
+      },
+      (err: any) => {
+        console.log(err);
+      },
+    );
+  }
+
+  private transformDataMovie(data: Imovie): Imovie {
+    const { _id, title, director, description, duration, image, genere } = data;
+
+    let auxMovie: Imovie = {
+      _id,
+      title,
+      director,
+      description,
+      duration,
+      image,
+      genere,
+    };
+    return auxMovie;
+  }
+
+
+  public getAuditorium(id: string){
+
+    this.bookingService.getAuditoriumByMovieId(id).subscribe(
+      (data: any)=>{
+        console.log("AQUI TIENE TUS PUTAS SALAS-->",data); //TODO: transformar los datos del back
+      },
+      (err: any)=>{
+        console.error(err);
+      }
+    )
+
+  }
+
+
+
 
 }
